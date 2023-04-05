5de6eaf0f4d620584cd5f708800beae669751d57
Parker M <99286695+parkzar@users.noreply.github.com>
2023-04-05 11:13:09 -0700

    Updated Database.java
    Once ran, it will create a table "company" in the postgres database. Access table via terminal if changes are needed to be made.
    
    Also changed password for user "postgres" and pgAdmin.

--- Database.java
+++ Database.java
@@ -3,19 +3,38 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+//Once this has been run, you'll need to use terminal to manipulate it 
 public class Database {
 	public static void main (String[] args) {
 		Connection c = null;
-		Statement stat = null;
+		Statement stmt = null;
 		
 		try {
 			Class.forName("org.postgresql.Driver");
 			c = DriverManager.getConnection(
 					"jdbc:postgresql://localhost:5432/",
-					"postgres", "cheese99"); //Change password ASAP because others will see this code 
+					"postgres", "pgAdminMasterPass!"); //Created table in "postgres" database
 			System.out.println("Connected to the database.");
 		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		
+		try {
+			stmt = c.createStatement();
+			String sql = "CREATE TABLE COMPANY " + 
+			"(ID INT PRIMARY KEY NOT NULL," + 
+			" NAME TEXT NOT NULL," + 
+			"AGE INT NOT NULL, " + 
+			" ADDRESS CHAR(50), " + 
+			" SALARY REAL)";
+			stmt.executeUpdate(sql);
+			stmt.close();
+			c.close();
+			System.out.println("Table has been created.");
+			
+		} catch (Exception e){
 			e.printStackTrace();
 			System.err.println(e.getClass().getName() + ": " + e.getMessage());
 			System.exit(0);
