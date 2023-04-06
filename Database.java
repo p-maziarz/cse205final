e6b253fb37fb1cba9aa8acc17871e6bea9003b1b
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-04-06 09:16:29 -0700

    Updated Database.java
    Practiced updating elements in the database.

--- Database.java
+++ Database.java
@@ -79,8 +79,6 @@ public class Database {
 			System.exit(0);
 		}
 		
-		*/
-		
 		//Select
 		
 		try { //Because we're only doing queries, no insertion, we don't need to set the autocommit to false 
@@ -108,7 +106,6 @@ public class Database {
 			while (rs.next()) { //while rs.next has an element inside of it
 				String name = rs.getString("name");
 				int age = rs.getInt("age");
-				
 				System.out.println("NAME: " + name);
 				System.out.println("AGE: " + age);
 				System.out.println();
@@ -119,6 +116,45 @@ public class Database {
 			rs.close();
 			stmt.close();
 			c.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+		
+		*/
+		
+		//Update
+		
+		try {
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+			String sql = "UPDATE COMPANY set SALARY = 30000.00 where ID = 3;";
+			stmt.executeUpdate(sql);
+			c.commit();
+			
+			ResultSet rs = stmt.executeQuery("select * from company;"); //ResultSet allows to grab information and bring it back and print it out
+			
+			while (rs.next()) { //while rs.next has an element inside of it
+				int id = rs.getInt("id");
+				String name = rs.getString("name");
+				int age = rs.getInt("age");
+				String address = rs.getString("address");
+				float salary = rs.getFloat("salary");
+				
+				System.out.println("ID: " + id);
+				System.out.println("NAME: " + name);
+				System.out.println("AGE: " + age);
+				System.out.println("ADDRESS: " + address); //put in single quotes when getting from terminal bc its a string 
+				System.out.println("SALARY: " + salary);
+				System.out.println();
+				
+			}
+			rs.close();
+			stmt.close();
+			c.close();
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
