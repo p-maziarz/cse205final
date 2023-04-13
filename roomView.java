ade961fc1fc625655512297ede9d35fef455ee55
Parker M <99286695+p-maziarz@users.noreply.github.com>
2023-04-13 09:22:45 -0700

    Updated /list command
    Currently working on /list command

--- roomView.java
+++ roomView.java
@@ -1,8 +1,13 @@
-//Monday, April 10th 2023 Room View
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 public class roomView {
 	public static void main(String[] args) {
-		String username; //this is an arbitrary value that will be changed later, just used as a placeholder
+		Connection c = null;
+		Statement stmt = null;
+		String username = "username"; //this is an arbitrary value that will be changed later, just used as a placeholder
 	        
 	        while (true) { //Change to while view is room 
 	            // Display chat room view
@@ -16,16 +21,40 @@ public class roomView {
 	                
 	                switch (command[0]) {
 	                    case "/list":
-	                        // Returns a list of users currently in this chat room.
+	                        /*
+	                        PER CHATGPT SUGGESTION:
+	                    	You can keep track of the users currently in the chat room using a data structure such as a list or a set. 
+	                    	You can then print the list of users to display the users currently in the chat room.
+	                    	*/
 	                    	System.out.println("Here are the current users logged into this chat room: ");
+						try {
+							stmt = c.createStatement();
+							ResultSet rs = stmt.executeQuery("select * from database;");
+							while (rs.next()) { //checks if rs.next has a value
+								String users = rs.getString("user"); //grabs the usernames of everyone logged in the chat room
+								System.out.println("USERNAME: " + users);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+							System.err.println(e.getClass().getName() + ": " + e.getMessage());
+							System.exit(0);
+						}
 	                        break;
 	                    case "/leave":
-	                        // Exits the chat room
+	                    	/*
+	                    	PER SUGGESTION:
+	                    	You can use a flag or a boolean variable to indicate that the user has left the chat room.  
+	                    	You can then break out of the while loop or use another approach to exit the chat room gracefully.
+	                    	*/
 	                    	System.out.println("You have exited the chat room.");
 	                    	scnr.close();
 	                        break;
 	                    case "/history":
-	                        // Prints all past messages for the room
+	                        /*
+	                        PER SUGGESTION:
+	                        You can store the chat messages in a data structure such as an array, a list, or a queue. 
+	                        You can then iterate through the data structure to print all the past messages for the chat room.
+	                        */
 	                    	System.out.println("Here are all of the past messages sent in this chat room: ");
 	                        break;
 	                    case "/help":
@@ -46,5 +75,5 @@ public class roomView {
 	        }
 	    }
 	    
-	    // Implement methods to handle various commands, if needed
+	    //Maybe implement a try-catch statement for the entire program
 	}
